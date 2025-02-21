28da5d3e775125ae76c638c7c76fd148dc74b636
Sooraj M S <soorajms4@gmail.com>
2025-02-21 19:24:53 +0530

    complete homepage UI design

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
 
-const TitleCards = () => {
+const TitleCards = ({title, category}) => {
+  const cardsRef = useRef();
+
+  const handleWheel = (e)=>{
+    e.preventDefault();
+    cardsRef.current.scrollLeft += e.deltaY;
+  }
+  useEffect(()=>{
+    cardsRef.current.addEventListener('wheel', handleWheel)
+  },[]);
+
   return (
-    <div className='titlecards'>
-      
+    <div className='title-cards'>
+      <h2>{title ? title : 'Popular on Netflix'}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index)=>{
+          return <div className="card" key={index}>
+            <img src={card.image} alt="" />
+            <p>{card.name}</p>
+          </div>
+        })}
+      </div>
     </div>
   )
 }
